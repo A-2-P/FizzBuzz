127af9a4b2dadb6c002b8b7c29ad084e18d7947c
Anel <anelpjanic@gmx.de>
2020-04-01 13:40:42 +0200

    Added Tests - Status almost GREEN

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -4,7 +4,22 @@ public class FizzBuzz implements IFizzBuzz {
 
     @Override
     public String fizzbuzz(int n) {
-        String result = "";
+        String result = " ";
+
+        for (int i = 1; i <= n; i++) {
+            if(i%3 == 0 && i%5 == 0){
+                result += " Fizz Buzz,";
+            }else {
+                if (i % 3 == 0) {
+                    result += " Fizz,";
+                } else if (i % 5 == 0) {
+                    result += " Buzz,";
+                } else {
+                    result =result + " "+i+",";
+                }
+            }
+
+        }
 
 
         return result;
