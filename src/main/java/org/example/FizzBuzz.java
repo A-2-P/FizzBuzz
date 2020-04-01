225b2fbec1e3c34ad87a24bf643237ded0bde8e4
Anel <anelpjanic@gmx.de>
2020-04-01 13:22:14 +0200

    Added initial FizzBuzz, IFizzBuzz and FizzBuzzTest

---
+++ src/main/java/org/example/FizzBuzz.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class FizzBuzz implements IFizzBuzz {
+
+    @Override
+    public String fizzbuzz(int n) {
+        String result = "";
+
+
+        return result;
+    }
+}
---
+++ src/main/java/org/example/IFizzBuzz.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public interface IFizzBuzz {
+    String fizzbuzz(int n);
+}
