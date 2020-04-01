44cc2a151c64715d925046d6f0eca603c91df4bf
Anel <anelpjanic@gmx.de>
2020-04-01 13:47:09 +0200

    Tests to GREEN

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -4,18 +4,22 @@ public class FizzBuzz implements IFizzBuzz {
 
     @Override
     public String fizzbuzz(int n) {
-        String result = " ";
+        String result = "";
 
         for (int i = 1; i <= n; i++) {
             if(i%3 == 0 && i%5 == 0){
-                result += " Fizz Buzz,";
+                result += "Fizz Buzz";
+                if(i!=n) result += ", ";
             }else {
                 if (i % 3 == 0) {
-                    result += " Fizz,";
+                    result += "Fizz";
+                    if(i!=n) result += ", ";
                 } else if (i % 5 == 0) {
-                    result += " Buzz,";
+                    result += "Buzz";
+                    if(i!=n) result += ", ";
                 } else {
-                    result =result + " "+i+",";
+                    result =result +""+i;
+                    if(i!=n) result += ", ";
                 }
             }
 
--- src/test/java/org/example/FizzBuzzTest.java
+++ src/test/java/org/example/FizzBuzzTest.java
@@ -15,17 +15,17 @@ public class FizzBuzzTest {
 
     @Test
     public void FizzBuzzTest_1(){
-        String result = " 1";
+        String result = "1";
         assertThat(result).isEqualTo(fizzy.fizzbuzz(1));
     }
     @Test
     public void FizzBuzzTest_7(){
-        String result = " 1, 2, Fizz, 4, Buzz, Fizz, 7";
+        String result = "1, 2, Fizz, 4, Buzz, Fizz, 7";
         assertThat(result).isEqualTo(fizzy.fizzbuzz(7));
     }
     @Test
     public void FizzBuzzTest_36(){
-        String result = " 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, Fizz Buzz, 31, 32, Fizz, 34, Buzz, Fizz";
+        String result = "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, Fizz Buzz, 31, 32, Fizz, 34, Buzz, Fizz";
         assertThat(result).isEqualTo(fizzy.fizzbuzz(36));
     }
 
